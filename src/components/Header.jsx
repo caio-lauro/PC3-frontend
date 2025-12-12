b294bc7ca50f089cae9de79df88a1e1e9751ec54
caio-lauro <caiolaurodelima@gmail.com>
2025-12-12 11:53:46 -0300

    atualizar header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,12 +6,12 @@ export default function Header() {
 	const [loggedIn, setLoggedIn] = useState(false);
 
 	useEffect(() => {
-        async function getLoggedIn() {
-            const logged = await isLoggedIn();
-            setLoggedIn(logged);
-        }
-		
-        getLoggedIn();
+		async function getLoggedIn() {
+			const logged = await isLoggedIn();
+			setLoggedIn(logged);
+		}
+
+		getLoggedIn();
 	}, []);
 
 	return (
@@ -33,33 +33,39 @@ export default function Header() {
 
 					{loggedIn ? (
 						<div className="flex items-center gap-4">
-                            <div className="sm:flex sm:gap-4">
-                                <a
-                                    className="block rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-teal-700"
-                                    href="/pedido"
-                                >
-                                    Fazer Pedido
-                                </a>
-                            </div>
-                        </div>
+							<div className="sm:flex sm:gap-4">
+								<a
+									className="block rounded-md bg-red-500 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-red-700"
+									href="/pedido"
+								>
+									Fazer Pedido
+								</a>
+								<a
+									className="block rounded-md bg-red-500 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-red-700"
+									href="/lista"
+								>
+									Meus pedidos
+								</a>
+							</div>
+						</div>
 					) : (
-                        <div className="flex items-center gap-4">
-                            <div className="sm:flex sm:gap-4">
-                                <a
-                                    className="block rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-teal-700"
-                                    href="/login"
-                                >
-                                    Login
-                                </a>
+						<div className="flex items-center gap-4">
+							<div className="sm:flex sm:gap-4">
+								<a
+									className="block rounded-md bg-red-400 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-red-600"
+									href="/login"
+								>
+									Login
+								</a>
 
-                                <a
-                                    className="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600 transition hover:text-teal-600/75 sm:block"
-                                    href="/cadastrar"
-                                >
-                                    Register
-                                </a>
-                            </div>
-                        </div>
+								<a
+									className="hidden rounded-md border border-red-600 bg-slate-100 px-5 py-2.5 text-sm font-medium text-red-500 transition hover:text-red-700 hover:bg-slate-300 sm:block"
+									href="/cadastrar"
+								>
+									Cadastrar
+								</a>
+							</div>
+						</div>
 					)}
 				</div>
 			</div>
