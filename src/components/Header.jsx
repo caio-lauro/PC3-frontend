4f096291fb193cb48640c0278c82aa3b7f8b73a1
caio-lauro <caiolaurodelima@gmail.com>
2025-12-13 12:25:34 -0300

    mudanças de estilização

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,21 @@ import isLoggedIn from "../api/utils";
 export default function Header() {
 	const [loggedIn, setLoggedIn] = useState(false);
 
+	const [isHovering, setIsHovering] = useState({
+		pedido: false,
+		lista: false
+	});
+
+	const handleHovering = (e) => {
+		const field = e.target.id;
+		const state = e._reactName === "onMouseEnter" ? true : false;
+
+		setIsHovering({
+			...isHovering,
+			[field]: state
+		});
+	}
+
 	useEffect(() => {
 		async function getLoggedIn() {
 			const logged = await isLoggedIn();
@@ -33,18 +48,99 @@ export default function Header() {
 
 					{loggedIn ? (
 						<div className="flex items-center gap-4">
-							<div className="sm:flex sm:gap-4">
+							<div className="sm:flex sm:gap-2">
 								<a
-									className="block rounded-md bg-red-500 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-red-700"
+									className="flex items-center gap-x-2 rounded-md bg-red-500 px-2.5 py-2.5 text-sm font-medium text-white 
+										transition delay-50 duration-300 ease-in-out hover:scale-110 hover:bg-red-700"
 									href="/pedido"
+									id="pedido"
+									onMouseEnter={handleHovering}
+									onMouseLeave={handleHovering}
 								>
-									Fazer Pedido
+									<svg
+										width="24px"
+										height="24px"
+										viewBox="0 0 24 24"
+										fill="white"
+									>
+										<circle cx="16.5" cy="18.5" r="1.5" />
+										<circle cx="9.5" cy="18.5" r="1.5" />
+										<path d="M18 16H8a1 1 0 0 1-.958-.713L4.256 6H3a1 1 0 0 1 0-2h2a1 1 0 0 1 .958.713L6.344 6H21a1 1 0 0 1 .937 1.352l-3 8A1 1 0 0 1 18 16zm-9.256-2h8.563l2.25-6H6.944z" />
+									</svg>
+									
+									<span 
+										className={`
+											${isHovering.pedido ? "opacity-100" : "opacity-0 -ml-2 max-w-0 max-h-0"}
+											transition-all duration-500 ease-in-out overflow-hidden font-bold
+										`}
+									>
+										Fazer Pedido
+									</span>
 								</a>
+
 								<a
-									className="block rounded-md bg-red-500 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-red-700"
+									className="flex items-center gap-x-2 rounded-md bg-red-500 px-2.5 py-2.5 text-sm font-medium text-white 
+										transition delay-50 duration-300 ease-in-out hover:scale-110 hover:bg-red-700"
 									href="/lista"
+									id="lista"
+									onMouseEnter={handleHovering}
+									onMouseLeave={handleHovering}
 								>
-									Meus pedidos
+									<svg
+										width="24px"
+										height="24px"
+										viewBox="0 0 24 12"
+									>
+										<rect
+											x="5px"
+											y="-4px"
+											rx="2px"
+											ry="2px"
+											width="14px"
+											height="20px"
+											fill="none"
+											stroke="white"
+											strokeWidth="2px"
+										/>
+
+										<rect 
+											width="8px"
+											height="1px"
+											x="8px"
+											y="1px"
+											fill="white"
+										/>
+										<rect 
+											width="8px"
+											height="1px"
+											x="8px"
+											y="4px"
+											fill="white"
+										/>
+										<rect 
+											width="8px"
+											height="1px"
+											x="8px"
+											y="7px"
+											fill="white"
+										/>
+										<rect 
+											width="8px"
+											height="1px"
+											x="8px"
+											y="10px"
+											fill="white"
+										/>
+									</svg>
+
+									<span 
+										className={`
+											${isHovering.lista ? "opacity-100" : "opacity-0 -ml-2 max-w-0 max-h-0"}
+											transition-all duration-500 ease-in-out overflow-hidden font-bold
+										`}
+									>
+										Pedidos
+									</span>
 								</a>
 							</div>
 						</div>
@@ -52,14 +148,16 @@ export default function Header() {
 						<div className="flex items-center gap-4">
 							<div className="sm:flex sm:gap-4">
 								<a
-									className="block rounded-md bg-red-400 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-red-600"
+									className="block rounded-md bg-red-400 px-5 py-2.5 text-sm font-medium text-white 
+										transition delay-50 duration-300 ease-in-out hover:scale-110 hover:bg-red-600"
 									href="/login"
 								>
 									Login
 								</a>
 
 								<a
-									className="hidden rounded-md border border-red-600 bg-slate-100 px-5 py-2.5 text-sm font-medium text-red-500 transition hover:text-red-700 hover:bg-slate-300 sm:block"
+									className="hidden rounded-md border border-red-600 bg-slate-100 px-5 py-2.5 text-sm font-medium text-red-500 
+										transition delay-50 duration-300 ease-in-out hover:scale-110 hover:text-red-700 hover:bg-slate-200 sm:block"
 									href="/cadastrar"
 								>
 									Cadastrar
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -20,13 +20,49 @@ export default function Index() {
 			<main className="h-screen">
 				<div className="flex content-center items-center justify-center h-screen">
 					<div className="p-30 bg-white/50 backdrop-blur-sm rounded-lg shadow-lg -translate-y-16">
-                        <h1 className="text-7xl text-orange-400 font-semibold">
-                            Que bom que está aqui!
-                        </h1>
 						{loggedIn ? (
-							<></>
+							<>
+								<h1 className="text-7xl text-orange-400 text-center font-semibold">
+									É bom te ver de novo!
+								</h1>
+
+								<p className="mt-8 mb-6 text-5xl text-center text-orange-400">
+									Vamos fazer um novo pedido?
+								</p>
+
+								<div className="mt-5 justify-center flex items-center gap-4">
+									<a
+										className="flex items-center gap-x-2 rounded-md bg-red-500 px-2.5 py-2.5 text-2xl text-white transition hover:bg-red-700"
+										href="/pedido"
+									>
+										<svg
+											width="36px"
+											height="36px"
+											viewBox="0 0 24 24"
+											fill="white"
+										>
+											<circle
+												cx="16.5"
+												cy="18.5"
+												r="1.5"
+											/>
+											<circle
+												cx="9.5"
+												cy="18.5"
+												r="1.5"
+											/>
+											<path d="M18 16H8a1 1 0 0 1-.958-.713L4.256 6H3a1 1 0 0 1 0-2h2a1 1 0 0 1 .958.713L6.344 6H21a1 1 0 0 1 .937 1.352l-3 8A1 1 0 0 1 18 16zm-9.256-2h8.563l2.25-6H6.944z" />
+										</svg>
+										<span>Fazer Pedido</span>
+									</a>
+								</div>
+							</>
 						) : (
 							<>
+								<h1 className="text-7xl text-orange-400 text-center font-semibold">
+									Que bom que está aqui!
+								</h1>
+
 								<p className="mt-15 mb-8 text-6xl text-center text-orange-400">
 									Vamos começar?
 								</p>
