c249f26364c3574df1197b9d74ea579cfc1828ab
caio-lauro <caiolaurodelima@gmail.com>
2025-12-13 14:30:54 -0300

    redirecionar se está logado

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -1,66 +1,136 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import Header from "../components/Header";
+import isLoggedIn from "../api/utils";
 
 export default function Cadastro() {
-    return (
-        <>
-            <Header />
-            <main className="h-screen">
-                <div className="flex content-center items-center justify-center h-screen">
-                    <form className="bg-white/80 border border-black/10 backdrop-blur-sm shadow-lg rounded pt-4 px-8 pb-4 mb-4 -translate-y-16" method="POST">
-                        <p className="text-xl text-gray-700 text-center mb-4">
-                            Informe seus dados para cadastrar-se.
-                        </p>
+    const navigate = useNavigate();
 
-                        <div className="mb-4">
-                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
-                                Nome
-                            </label>
-                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" placeholder="Nome" />
-                        </div>
+	useEffect(() => {
+		async function navigateIfLogged() {
+			const logged = await isLoggedIn();
+			if (logged) {
+				navigate("/", { replace: true });
+			}
+		}
 
-                        <div className="mb-4">
-                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="phone">
-                                Telefone
-                            </label>
-                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="phone" type="text" placeholder="Telefone" />
-                        </div>
+        navigateIfLogged();
+	}, []);
 
-                        <div className="mb-4">
-                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="cep">
-                                CEP
-                            </label>
-                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="cep" type="text" placeholder="CEP" />
-                        </div>
+	return (
+		<>
+			<Header />
+			<main className="h-screen">
+				<div className="flex content-center items-center justify-center h-screen">
+					<form
+						className="bg-white/80 border border-black/10 backdrop-blur-sm shadow-lg rounded pt-4 px-8 pb-4 mb-4 -translate-y-16"
+						method="POST"
+					>
+						<p className="text-xl text-gray-700 text-center mb-4">
+							Informe seus dados para cadastrar-se.
+						</p>
 
-                        <div className="mb-4">
-                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-                                E-mail
-                            </label>
-                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="text" placeholder="E-mail" />
-                        </div>
+						<div className="mb-4">
+							<label
+								className="block text-gray-700 text-sm font-bold mb-2"
+								htmlFor="name"
+							>
+								Nome
+							</label>
+							<input
+								className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+								id="name"
+								type="text"
+								placeholder="Nome"
+							/>
+						</div>
 
-                        <div className="mb-4">
-                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-                                Senha
-                            </label>
-                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="********" />
-                        </div>
+						<div className="mb-4">
+							<label
+								className="block text-gray-700 text-sm font-bold mb-2"
+								htmlFor="phone"
+							>
+								Telefone
+							</label>
+							<input
+								className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+								id="phone"
+								type="text"
+								placeholder="Telefone"
+							/>
+						</div>
 
-                        <div className="mb-0">
-                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="confirmation">
-                                Confirmação da senha
-                            </label>
-                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="confirmation" placeholder="********" />
-                        </div>
+						<div className="mb-4">
+							<label
+								className="block text-gray-700 text-sm font-bold mb-2"
+								htmlFor="cep"
+							>
+								CEP
+							</label>
+							<input
+								className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+								id="cep"
+								type="text"
+								placeholder="CEP"
+							/>
+						</div>
 
-                        <div className="flex items-center justify-center">
-                            <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-                                Registrar
-                            </button>
-                        </div>
-                    </form>
-                </div>
-            </main>
-        </>
-    );
+						<div className="mb-4">
+							<label
+								className="block text-gray-700 text-sm font-bold mb-2"
+								htmlFor="email"
+							>
+								E-mail
+							</label>
+							<input
+								className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+								id="email"
+								type="text"
+								placeholder="E-mail"
+							/>
+						</div>
+
+						<div className="mb-4">
+							<label
+								className="block text-gray-700 text-sm font-bold mb-2"
+								htmlFor="password"
+							>
+								Senha
+							</label>
+							<input
+								className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+								id="password"
+								type="password"
+								placeholder="********"
+							/>
+						</div>
+
+						<div className="mb-0">
+							<label
+								className="block text-gray-700 text-sm font-bold mb-2"
+								htmlFor="confirmation"
+							>
+								Confirmação da senha
+							</label>
+							<input
+								className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+								id="password"
+								type="confirmation"
+								placeholder="********"
+							/>
+						</div>
+
+						<div className="flex items-center justify-center">
+							<button
+								className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+								type="submit"
+							>
+								Registrar
+							</button>
+						</div>
+					</form>
+				</div>
+			</main>
+		</>
+	);
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,38 +1,76 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import Header from "../components/Header";
+import isLoggedIn from "../api/utils";
 
 export default function Login() {
-    return (
-        <>
-            <Header />
-            <main className="h-screen">
-                <div className="flex content-center items-center justify-center h-screen">
-                    <form className="bg-white/80 border border-black/10 backdrop-blur-sm shadow-lg rounded pt-4 px-8 pb-4 mb-4 -translate-y-16" method="POST">
-                        <p className="text-xl text-gray-700 text-center mb-4">
-                            Informe seus dados para fazer login.
-                        </p>
-                        
-                        <div className="mb-4">
-                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-                                E-mail
-                            </label>
-                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="text" placeholder="E-mail" />
-                        </div>
+	const navigate = useNavigate();
 
-                        <div className="mb-4">
-                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-                                Senha
-                            </label>
-                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="********" />
-                        </div>
+	useEffect(() => {
+		async function navigateIfLogged() {
+			const logged = await isLoggedIn();
+			if (logged) {
+				navigate("/", { replace: true });
+			}
+		}
 
-                        <div className="flex items-center justify-center">
-                            <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-                                Entrar
-                            </button>
-                        </div>
-                    </form>
-                </div>
-            </main>
-        </>
-    );
+        navigateIfLogged();
+	}, []);
+
+	return (
+		<>
+			<Header />
+			<main className="h-screen">
+				<div className="flex content-center items-center justify-center h-screen">
+					<form
+						className="bg-white/80 border border-black/10 backdrop-blur-sm shadow-lg rounded pt-4 px-8 pb-4 mb-4 -translate-y-16"
+						method="POST"
+					>
+						<p className="text-xl text-gray-700 text-center mb-4">
+							Informe seus dados para fazer login.
+						</p>
+
+						<div className="mb-4">
+							<label
+								className="block text-gray-700 text-sm font-bold mb-2"
+								htmlFor="email"
+							>
+								E-mail
+							</label>
+							<input
+								className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+								id="email"
+								type="text"
+								placeholder="E-mail"
+							/>
+						</div>
+
+						<div className="mb-4">
+							<label
+								className="block text-gray-700 text-sm font-bold mb-2"
+								htmlFor="password"
+							>
+								Senha
+							</label>
+							<input
+								className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+								id="password"
+								type="password"
+								placeholder="********"
+							/>
+						</div>
+
+						<div className="flex items-center justify-center">
+							<button
+								className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+								type="submit"
+							>
+								Entrar
+							</button>
+						</div>
+					</form>
+				</div>
+			</main>
+		</>
+	);
 }
