b213521700a2f91eb705e3110dc6d4bbfe3bb9d0
caio-lauro <caiolaurodelima@gmail.com>
2025-12-14 18:43:02 -0300

    finalizar cadastro

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -1,10 +1,93 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Header from "../components/Header";
 import isLoggedIn from "../api/utils";
 
 export default function Cadastro() {
     const navigate = useNavigate();
+
+	const [error, setError] = useState("");
+
+	const [formData, setFormData] = useState({
+		name: "",
+		phone: "",
+		cep: "",
+		email: "",
+		password: "",
+		confirm: ""
+	});
+
+	const handleChange = (e) => {
+		const id = e.target.id;
+		const { name, value } = e.target;
+
+		setError("");
+
+		setFormData({
+			...formData,
+			[id]: value
+		});
+
+		if (value.trim() === "") {
+			setError(`O campo ${name} não pode ficar vazio!`);
+		}
+
+		if (id === "name") {
+			if (value.length() < 3) {
+				setError(`O ${name} deve possuir mais de 3 letras.`);
+			} else if (!/^\p{L}+$/u.test(value)) {
+				setError(`O ${name} deve possuir apenas letras do alfabeto.`);
+			}
+		} else if (id === "phone") {
+			const phoneRegex = /^([0-9]{2})([9]{0,1})([0-9]{4,5})([0-9]{4})$/;
+			let str = value.replace(/[^0-9]/g, "").slice(0, 11);
+
+			const result = str.replace(phoneRegex, "($1) $2 $3-$4");
+
+			setFormData({
+				...formData,
+				[id]: result
+			})
+		} else if (id === "cep") {
+			const cepRegex = /^([0-9]{5})([0-9]{3})$/;
+			let str = value.replace(/[^0-9]/g, "").slice(0, 8);
+
+			const result = str.replace(cepRegex, "$1-$2");
+
+			setFormData({
+				...formData,
+				[id]: result
+			});
+		} else if (id === "email" && !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)) {
+			setError(`O ${name} deve ser válido.`);
+		} else if (id === "confirm" && formData.password !== value) {
+			setError("Senha e confirmação devem coincidir.");
+		}
+	}
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		if (error) return;
+
+		try {
+			const response = await fetch('http://localhost:3000/api/cadastrar', {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify(formData)
+			});
+
+			const data = await response.json();
+
+			if (!response.ok) {
+				setError(response.error);
+			}
+
+			navigate("/login", { replace: true });
+		} catch (err) {
+			console.error(err);
+		}
+	}
 
 	useEffect(() => {
 		async function navigateIfLogged() {
@@ -25,6 +108,7 @@ export default function Cadastro() {
 					<form
 						className="bg-white/80 border border-black/10 backdrop-blur-sm shadow-lg rounded pt-4 px-8 pb-4 mb-4"
 						method="POST"
+						onSubmit={handleSubmit}
 					>
 						<p className="text-xl text-gray-700 text-center mb-4">
 							Informe seus dados para cadastrar-se.
@@ -40,8 +124,11 @@ export default function Cadastro() {
 							<input
 								className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
 								id="name"
+								name="nome"
 								type="text"
 								placeholder="Nome"
+								value={formData.name}
+								onChange={handleChange}
 							/>
 						</div>
 
@@ -55,8 +142,11 @@ export default function Cadastro() {
 							<input
 								className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
 								id="phone"
+								name="telefone"
 								type="text"
 								placeholder="Telefone"
+								value={formData.phone}
+								onChange={handleChange}
 							/>
 						</div>
 
@@ -70,8 +160,11 @@ export default function Cadastro() {
 							<input
 								className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
 								id="cep"
+								name="CEP"
 								type="text"
 								placeholder="CEP"
+								value={formData.cep}
+								onChange={handleChange}
 							/>
 						</div>
 
@@ -85,8 +178,11 @@ export default function Cadastro() {
 							<input
 								className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
 								id="email"
+								name="e-mail"
 								type="text"
 								placeholder="E-mail"
+								value={formData.email}
+								onChange={handleChange}
 							/>
 						</div>
 
@@ -100,8 +196,11 @@ export default function Cadastro() {
 							<input
 								className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
 								id="password"
+								name="senha"
 								type="password"
 								placeholder="********"
+								value={formData.password}
+								onChange={handleChange}
 							/>
 						</div>
 
@@ -114,11 +213,20 @@ export default function Cadastro() {
 							</label>
 							<input
 								className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-								id="password"
-								type="confirmation"
+								id="confirm"
+								name="confirmação da senha"
+								type="password"
 								placeholder="********"
+								value={formData.confirm}
+								onChange={handleChange}
 							/>
 						</div>
+
+						{error && 
+							<p className="mb-2 font-sm text-red-600">
+								{error}
+							</p>
+						}
 
 						<div className="flex items-center justify-center">
 							<button
